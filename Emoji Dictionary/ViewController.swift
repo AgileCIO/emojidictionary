335ef854d15743ebe93441606237282ecf89babe
DAVID MANN <david@datmobi.com>
2017-09-24 19:07:10 -0700

    Second commit for this project (Emoji Dictionary).

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // some changes
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
