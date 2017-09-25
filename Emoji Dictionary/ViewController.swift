d42226ecfeb9f3c6ff6a7ef0643c48340b8985b3
DAVID MANN <david@datmobi.com>
2017-09-24 19:53:39 -0700

    Added the table view with 10 emojis

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // some changes
+    @IBOutlet weak var productsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        productsTableView.dataSource = self
+        productsTableView.delegate = self
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // how many rows in the table view
+        
+        return 10
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // put items in the table view row or cell
+        let cell = UITableViewCell()
+    
+        cell.textLabel?.text = "😎"
+        
+        
+        return cell
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
